4f950870487940adf71fc7c813a0f854a1ef40d0
AlexeyKarasev <karasev.alexey@gmail.com>
2018-09-26 15:18:35 +0300

    extracted common logic

--- src/client/storiqa/mod.rs
+++ src/client/storiqa/mod.rs
@@ -9,6 +9,7 @@ use futures::prelude::*;
 use hyper::Method;
 use hyper::{Body, Request};
 use models::*;
+use serde::Deserialize;
 use serde_json;
 use std::sync::Arc;
 use utils::read_body;
@@ -29,6 +30,25 @@ impl StoriqaClientImpl {
             storiqa_url: config.client.storiqa_url.clone(),
         }
     }
+
+    fn exec_query<'de, T: Deserialize<'de>>(&self, query: &str) -> impl Future<Item = T, Error = Error> {
+        let query = query.to_string();
+        let query1 = query.clone();
+        let query2 = query.clone();
+        let query3 = query.clone();
+        let cli = self.cli.clone();
+        Request::builder()
+            .method(Method::POST)
+            .body(Body::from(query))
+            .map_err(move |e| error_context!(e, ErrorKind::Http, query3))
+            .into_future()
+            .and_then(move |req| cli.request(req).map_err(move |e| error_context!(e, ErrorKind::Hyper, query1)))
+            .and_then(move |resp| read_body(resp.into_body()).map_err(move |e| error_context!(e, ErrorKind::Hyper, query2)))
+            .and_then(|bytes| {
+                let bytes_clone = bytes.clone();
+                String::from_utf8(bytes).map_err(move |e| error_context!(e, ErrorKind::UTF8, bytes_clone))
+            }).and_then(|string| serde_json::from_str::<T>(&string).map_err(|e| error_context!(e, ErrorKind::Json, string)))
+    }
 }
 
 impl StoriqaClient for StoriqaClientImpl {
@@ -43,23 +63,8 @@ impl StoriqaClient for StoriqaClientImpl {
             "#,
             email, password
         );
-        let query1 = query.clone();
-        let query2 = query.clone();
-        let query3 = query.clone();
-        let cli = self.cli.clone();
         Box::new(
-            Request::builder()
-                .method(Method::POST)
-                .body(Body::from(query))
-                .map_err(move |e| error_context!(e, ErrorKind::Http, query3))
-                .into_future()
-                .and_then(move |req| cli.request(req).map_err(move |e| error_context!(e, ErrorKind::Hyper, query1)))
-                .and_then(move |resp| read_body(resp.into_body()).map_err(move |e| error_context!(e, ErrorKind::Hyper, query2)))
-                .and_then(|bytes| {
-                    let bytes_clone = bytes.clone();
-                    String::from_utf8(bytes).map_err(move |e| error_context!(e, ErrorKind::UTF8, bytes_clone))
-                })
-                .and_then(|string| serde_json::from_str::<GetJWTResponse>(&string).map_err(|e| error_context!(e, ErrorKind::Json, string)))
+            self.exec_query::<GetJWTResponse>(&query)
                 .and_then(|resp| resp.data.ok_or(ErrorKind::Unauthorized.into()))
                 .map(|resp_data| StoriqaJWT::new(resp_data.getJWTByEmail.token)),
         )
