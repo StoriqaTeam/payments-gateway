fe6dcd97f60db48c1e3d9f34e7491b1de456a092
Serejka <5125402@mail.ru>
2018-10-03 11:14:00 +0300

    fixed new user validation

--- src/models/password.rs
+++ src/models/password.rs
@@ -1,5 +1,8 @@
+use std::borrow::Cow;
+use std::collections::HashMap;
 use std::fmt::{Debug, Error, Formatter};
 
+use regex::Regex;
 use serde::{Serialize, Serializer};
 use validator::{Validate, ValidationErrors};
 
@@ -7,6 +10,10 @@ use validator::{Validate, ValidationErrors};
 pub struct Password(String);
 
 const PASSWORD_MASK: &str = "********";
+
+lazy_static! {
+    static ref REG_CONTAINS_NUMBERS: Regex = Regex::new(r#"\d"#).unwrap();
+}
 
 impl Debug for Password {
     fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
@@ -26,13 +33,33 @@ impl Serialize for Password {
 impl Validate for Password {
     fn validate(&self) -> Result<(), ValidationErrors> {
         let password_len = self.0.len();
+        let mut errors = ValidationErrors::new();
         if password_len < 8 || password_len > 30 {
-            let error = validation_errors!({
-                "password": ["len" => "Password should be between 8 and 30 symbols"]
-            });
-            Err(error)
-        } else {
+            let error = validator::ValidationError {
+                code: Cow::from("len"),
+                message: Some(Cow::from("Password should be between 8 and 30 symbols")),
+                params: HashMap::new(),
+            };
+            errors.add("password", error);
+        } else if self.0 == self.0.to_lowercase() {
+            let error = validator::ValidationError {
+                code: Cow::from("upper case"),
+                message: Some(Cow::from("Password should contain at least one upper case character")),
+                params: HashMap::new(),
+            };
+            errors.add("password", error);
+        } else if !REG_CONTAINS_NUMBERS.is_match(&self.0) {
+            let error = validator::ValidationError {
+                code: Cow::from("numbers"),
+                message: Some(Cow::from("Password should contain at least one number")),
+                params: HashMap::new(),
+            };
+            errors.add("password", error);
+        }
+        if errors.is_empty() {
             Ok(())
+        } else {
+            Err(errors)
         }
     }
 }
